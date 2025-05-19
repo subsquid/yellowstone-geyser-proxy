5a9fc235f830c019b610c70d22a69f5e10ca7359
Eldar Gabdullin <eldargab@gmail.com>
2025-05-19 17:22:27 +0300

    increase broadcast queue size

--- src/geyser_subscription.rs
+++ src/geyser_subscription.rs
@@ -38,7 +38,7 @@ impl GeyserSubscription {
 
         let updates = subscribe(&mut client).await?;
 
-        let tx = tokio::sync::broadcast::Sender::new(2);
+        let tx = tokio::sync::broadcast::Sender::new(10);
         let (subscribed_tx, subscribed_rx) = tokio::sync::watch::channel(());
 
         tokio::spawn(
